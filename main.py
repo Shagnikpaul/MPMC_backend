b20fc7dcae2818ba4af3da8a775375b00aea4789
Shagnik <shagnikpaul.772@gmail.com>
2025-03-30 16:08:03 +0530

    CORS fix

--- main.py
+++ main.py
@@ -3,7 +3,19 @@
 from fastapi import FastAPI, HTTPException
 from db import *
 from models import *
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
